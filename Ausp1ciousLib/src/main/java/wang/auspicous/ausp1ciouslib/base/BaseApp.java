6537c3e73e4bdb8faa946c501d997462c0bd857a
Ausp1cious <shawas@vip.qq.com>
2019-02-11 17:23:35 +0800

    添加BaseApplication

---
+++ Ausp1ciousLib/src/main/java/wang/auspicous/ausp1ciouslib/base/BaseApp.java
@@ -0,0 +1,25 @@
+package wang.auspicous.ausp1ciouslib.base;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by Ausp1cious on 2019/2/1.
+ */
+public class BaseApp extends Application {
+  private static BaseApp mInstance;
+  private static Context mContext;
+  public static Application getInstance() {
+    return mInstance;
+  }
+  public static Context getContext() {
+    return mContext;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    mInstance = this;
+    mContext = this;
+  }
+}
---
+++ app/src/main/java/wang/auspicous/ausp1cious/AppApplication.java
@@ -0,0 +1,10 @@
+package wang.auspicous.ausp1cious;
+
+import wang.auspicous.ausp1ciouslib.base.BaseApp;
+
+/**
+ * Created by Ausp1cious on 2019/2/1.
+ */
+public class AppApplication extends BaseApp {
+
+}
