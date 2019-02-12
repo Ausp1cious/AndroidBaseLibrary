0da4c8e9861867a7d06e9581201a797034405efe
Ausp1cious <shawas@vip.qq.com>
2019-02-12 10:32:32 +0800

    添加混淆和BaseBean

---
+++ Ausp1ciousLib/src/main/java/wang/auspicous/ausp1ciouslib/component/BaseBean.java
@@ -0,0 +1,10 @@
+package wang.auspicous.ausp1ciouslib.component;
+
+import java.io.Serializable;
+
+/**
+ * Created by Ausp1cious on 2019/2/12.
+ */
+public class BaseBean implements Serializable {
+
+}
