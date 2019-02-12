40a9bcb0a43f35829bcba9a6f0cb9bab54716e1b
Ausp1cious <shawas@vip.qq.com>
2019-02-12 14:40:01 +0800

    初始化Logger

--- Ausp1ciousLib/src/main/java/wang/auspicous/ausp1ciouslib/base/BaseApp.java
+++ Ausp1ciousLib/src/main/java/wang/auspicous/ausp1ciouslib/base/BaseApp.java
@@ -3,6 +3,11 @@ package wang.auspicous.ausp1ciouslib.base;
 import android.app.Application;
 import android.content.Context;
 
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.FormatStrategy;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
+
 /**
  * Created by Ausp1cious on 2019/2/1.
  */
@@ -21,5 +26,12 @@ public class BaseApp extends Application {
     super.onCreate();
     mInstance = this;
     mContext = this;
+    //初始化
+    initLogger();
+  }
+
+  private void initLogger() {
+    FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder().tag("BaseLog").build();
+    Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy));
   }
 }
