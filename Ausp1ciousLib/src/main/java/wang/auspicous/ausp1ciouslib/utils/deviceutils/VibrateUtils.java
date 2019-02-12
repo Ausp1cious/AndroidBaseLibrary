6b8ccd5ebd0074c66a78a1e67f9cfdd221194df5
Ausp1cious <shawas@vip.qq.com>
2019-02-12 09:40:08 +0800

    设备管理工具类细分

--- Ausp1ciousLib/src/main/java/wang/auspicous/ausp1ciouslib/utils/deviceutils/VibrateUtils.java
+++ Ausp1ciousLib/src/main/java/wang/auspicous/ausp1ciouslib/utils/deviceutils/VibrateUtils.java
@@ -1,12 +1,13 @@
-package wang.auspicous.ausp1ciouslib.utils;
+package wang.auspicous.ausp1ciouslib.utils.deviceutils;
 
 import android.content.Context;
 import android.os.Vibrator;
 
 /**
  * Created by Ausp1cious on 2019/1/31.
+ * 手机震动工具类
  */
-public class DeviceUtils {
+public class VibrateUtils {
   public static void vibrate(Context context, long duration) {
     Vibrator vibrator = (Vibrator) context.getApplicationContext().getSystemService(
             Context.VIBRATOR_SERVICE);
