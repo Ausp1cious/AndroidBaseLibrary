94d3472a49b613c1228e8d8f7762210dc4eeb917
Ausp1cious <shawas@vip.qq.com>
2019-02-15 09:36:59 +0800

    震动工具类添加取消震动选项

--- Ausp1ciousLib/src/main/java/wang/auspicous/ausp1ciouslib/utils/deviceutils/VibrateUtils.java
+++ Ausp1ciousLib/src/main/java/wang/auspicous/ausp1ciouslib/utils/deviceutils/VibrateUtils.java
@@ -1,13 +1,21 @@
 package wang.auspicous.ausp1ciouslib.utils.deviceutils;
 
+import android.Manifest;
 import android.content.Context;
 import android.os.Vibrator;
+
+import androidx.annotation.RequiresPermission;
 
 /**
  * Created by Ausp1cious on 2019/1/31.
  * 手机震动工具类
  */
 public class VibrateUtils {
+
+  /**
+   * 开始震动
+   */
+  @RequiresPermission(Manifest.permission.VIBRATE)
   public static void vibrate(Context context, long duration) {
     Vibrator vibrator = (Vibrator) context.getApplicationContext().getSystemService(
             Context.VIBRATOR_SERVICE);
@@ -15,6 +23,17 @@ public class VibrateUtils {
     if (vibrator != null) {
       vibrator.vibrate(pattern, -1);
     }
+  }
 
+  /**
+   * 取消震动
+   */
+  @RequiresPermission(Manifest.permission.VIBRATE)
+  public static void cancelVibrate(Context context) {
+    Vibrator vibrator = (Vibrator) context.getApplicationContext().getSystemService(
+            Context.VIBRATOR_SERVICE);
+    if (vibrator != null) {
+      vibrator.cancel();
+    }
   }
 }
