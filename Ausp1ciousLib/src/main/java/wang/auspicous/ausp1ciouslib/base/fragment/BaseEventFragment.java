9c4d712c616bbfb981b2053bcc75191f73b773d9
Ausp1cious <shawas@vip.qq.com>
2019-02-22 19:08:37 +0800

    BaseEventFragment Fragment事件接收

---
+++ Ausp1ciousLib/src/main/java/wang/auspicous/ausp1ciouslib/base/fragment/BaseEventFragment.java
@@ -0,0 +1,73 @@
+package wang.auspicous.ausp1ciouslib.base.fragment;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import wang.auspicous.ausp1ciouslib.component.eventbus.EventBusMessageCenter;
+import wang.auspicous.ausp1ciouslib.component.eventbus.EventBusStickMessageCenter;
+
+/**
+ * Created by Ausp1cious on 2019/2/22.
+ */
+public abstract class BaseEventFragment extends BaseNetEvnFragment {
+    @Override
+    public void onStart() {
+        super.onStart();
+        registerEventBus();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        unregisterEvenBus();
+    }
+    /**
+     * 是否允许使用EvenBus
+     * @return true-允许使用（默认） false-不允许使用
+     */
+    protected boolean enableEventBus() {
+        return true;
+    }
+
+    /**
+     * EventBus 消息接收的地方
+     * @param event 事件
+     * POSTING:从哪个线程出来，回到哪个线程
+     * MAIN: 执行在主线程(默认)
+     * BACKGROUND：执行在子线程
+     * ASYNC：创建新子线程执行
+     */
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onGetMessageEvent(EventBusMessageCenter event) {
+
+    }
+
+    /**
+     * EventBus stickMessage 执行的地方
+     * @param stickEvent stickMessage
+     */
+    @Subscribe(threadMode = ThreadMode.MAIN,sticky = true)
+    public void onGetStickMessageEvent(EventBusStickMessageCenter stickEvent) {
+
+    }
+
+    /**
+     * 注册EventBus
+     */
+    private void registerEventBus() {
+        if (enableEventBus() && !EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().register(this);
+        }
+    }
+
+    /**
+     * 注销EventBus
+     */
+    private void unregisterEvenBus() {
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this);
+        }
+    }
+
+}
