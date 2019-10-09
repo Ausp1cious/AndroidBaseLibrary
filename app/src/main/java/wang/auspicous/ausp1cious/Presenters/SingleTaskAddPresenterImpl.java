608a6c75e8d2a58b1ff9f1a82b591c7757f3abf4
Ausp1cious <Ausp1cious@auspicious.wang>
2019-10-09 17:32:16 +0800

    添加最小限定符

--- app/src/main/java/wang/auspicous/ausp1cious/Presenters/SingleTaskAddPresenterImpl.java
+++ app/src/main/java/wang/auspicous/ausp1cious/Presenters/SingleTaskAddPresenterImpl.java
@@ -3,6 +3,6 @@ package wang.auspicous.ausp1cious.Presenters;
 import wang.auspicous.ausp1cious.Presenters.Contracts.SingleTaskAddContract;
 import wang.auspicous.ausp1ciouslib.base.presenter.BasePresenterImpl;
 
-public class SingleTaskAddPrensenterImpl extends BasePresenterImpl<SingleTaskAddContract.SingleTaskAddView> implements SingleTaskAddContract.Presenter {
+public class SingleTaskAddPresenterImpl extends BasePresenterImpl<SingleTaskAddContract.SingleTaskAddView> implements SingleTaskAddContract.Presenter {
 
 }
--- app/src/main/java/wang/auspicous/ausp1cious/ui/task/SingleTaskAddActivity.java
+++ app/src/main/java/wang/auspicous/ausp1cious/ui/task/SingleTaskAddActivity.java
@@ -1,7 +1,7 @@
 package wang.auspicous.ausp1cious.ui.task;
 
 import wang.auspicous.ausp1cious.Presenters.Contracts.SingleTaskAddContract;
-import wang.auspicous.ausp1cious.Presenters.SingleTaskAddPrensenterImpl;
+import wang.auspicous.ausp1cious.Presenters.SingleTaskAddPresenterImpl;
 import wang.auspicous.ausp1cious.R;
 import wang.auspicous.ausp1cious.base.AppMVPActivity;
 
@@ -9,8 +9,7 @@ import wang.auspicous.ausp1cious.base.AppMVPActivity;
  * 单任务添加页面
  */
 public class SingleTaskAddActivity extends AppMVPActivity<SingleTaskAddContract.SingleTaskAddView
-        , SingleTaskAddPrensenterImpl> implements SingleTaskAddContract.SingleTaskAddView {
-
+        , SingleTaskAddPresenterImpl> implements SingleTaskAddContract.SingleTaskAddView {
     @Override
     protected int setContainerView() {
         return R.layout.activity_single_task_add;
